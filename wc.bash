00f0a92ec6510417f182caad6a2dad6626fc479f
Benjamin Biggs <benjbiggs@outlook.com>
2021-01-08 17:39:42 +0000

    incorporated first year report

--- wc.bash
+++ wc.bash
@@ -1,6 +1,18 @@
 #!/bin/bash
-numWords=$(ps2ascii thesis.pdf | wc -w)
+numWords=$(ps2ascii $1 | wc -w)
 totalWordsAllowed=65000
+alexWords=68182
+fotiosWords=50973
+yaniWords=57477
 percentComplete=$(echo "scale=2; (100 * $numWords) / $totalWordsAllowed" | bc)
+percentCompleteAlex=$(echo "scale=2; (100 * $numWords) / $alexWords" | bc)
+percentCompleteFotios=$(echo "scale=2; (100 * $numWords) / $fotiosWords" | bc)
+percentCompleteYani=$(echo "scale=2; (100 * $numWords) / $yaniWords" | bc)
 
-echo Num Words: $numWords of $totalWordsAllowed. Percent Complete: $percentComplete%
+echo "****************************************"
+echo "MAX_ALLOWED:" $numWords of $totalWordsAllowed: $percentComplete%
+echo "****************************************"
+echo "       ALEX:" $numWords of $alexWords: $percentCompleteAlex%
+echo "     FOTIOS:" $numWords of $fotiosWords: $percentCompleteFotios%
+echo "       YANI:" $numWords of $yaniWords: $percentCompleteYani%
+echo "****************************************"
