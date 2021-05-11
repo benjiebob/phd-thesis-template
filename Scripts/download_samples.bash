89e5ece6506f71c70cfa199ce6b87fca669ff00a
Ben Biggs <bjb10042@gskwmc02fg3mlmd6t.home>
2021-05-11 18:52:28 +0100

    did most of the smbld maths

---
+++ Scripts/download_samples.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+# mkdir Examples/
+wget -O Examples/kanazawa-thesis.pdf https://people.eecs.berkeley.edu/~kanazawa/papers/thesis.pdf
+wget -O Examples/kendall-thesis.pdf https://www.repository.cam.ac.uk/bitstream/handle/1810/287944/thesis.pdf?sequence=1&isAllowed=y
+wget -O Examples/yani-thesis.pdf https://www.repository.cam.ac.uk/bitstream/handle/1810/278976/thesisv2.pdf?sequence=7&isAllowed=y
+wget -O Examples/fotios-thesis.pdf https://www.repository.cam.ac.uk/bitstream/handle/1810/292450/fl302_Thesis_PhD.pdf?sequence=1&isAllowed=y
